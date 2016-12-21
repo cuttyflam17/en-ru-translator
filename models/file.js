880cb76781f566255bb4db14c51e6da7c9a71788
elaman <elaman.bazarov98@gmail.com>
2016-12-21 13:51:44 +0600

    done

--- models/file.js
+++ models/file.js
@@ -3,10 +3,10 @@ var fs=require("fs");
 module.exports=function(id, ip, callback){
 	console.log("token created");
 	if(ip === "::ffff:77.235.20.133") {
-    url = "http://77.235.20.133:3000"
+    url = "http://77.235.20.133:3040"
   }
   else {
-    url = "http://api.kamp.kg";
+    url = "http://files.kamp.kg";
    }
 request.post(url)
 .attach("file",__dirname+"/../sounds/"+id+".mp4").end(callback);
